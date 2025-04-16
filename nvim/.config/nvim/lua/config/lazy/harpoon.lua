3804759a315ba3720b0b42afa46bc72cf3dcfd77
jramber <jdramirezbernal@gmail.com>
2025-04-16 19:38:50 +0200

    fix harpoon setup

--- nvim/.config/nvim/lua/config/lazy/harpoon.lua
+++ nvim/.config/nvim/lua/config/lazy/harpoon.lua
@@ -1,16 +1,35 @@
 return {
     'theprimeagen/harpoon',
+    enabled = true,
     branch = "harpoon2",
-    config = function()
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
+    keys = function()
         local harpoon = require("harpoon")
 
-        vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        return {
+            { "<C-h>",     function() harpoon:list():select(1) end,                     desc = "Harpoon file 1" },
+            { "<C-t>",     function() harpoon:list():select(2) end,                     desc = "Harpoon file 2" },
+            { "<C-n>",     function() harpoon:list():select(3) end,                     desc = "Harpoon file 3" },
+            { "<C-s>",     function() harpoon:list():select(4) end,                     desc = "Harpoon file 4" },
 
-        vim.keymap.set("n", "<C-h>", function() harpoon.list().select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon.list().select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon.list().select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon.list().select(4) end)
+            -- { "",  function() harpoon:list():next() end, desc = "Harpoon navigates to next buffer" }
+            -- { "",  function() harpoon:list():prev() end, desc = "Harpoon navigates to previous buffer" }
+
+            { "<C-e>",     function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Harpoon menu" },
+            { "<leader>A", function() harpoon:list():prepend() end,                     desc = "Harpoon prepend" },
+            { "<leader>a", function() harpoon:list():add() end,                         desc = "Harpoon add" },
+
+            -- { "<C-E>",     function() toggle_telescope(harpoon:list()) end,             desc = "" }
+        }
+    end,
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon.setup({
+            menu = {
+                width = vim.api.nvim_win_get_width(0) - 4,
+            },
+        })
     end
 }
