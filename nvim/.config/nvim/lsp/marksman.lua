9f38d3afe237cd96d193b46521e486b906d19a59
jramber <jdramirezbernal@gmail.com>
2025-07-20 21:28:11 +0200

    unify and simplify part of the conf

--- nvim/.config/nvim/lsp/marksman.lua
+++
@@ -1,5 +0,0 @@
-vim.lsp.config['marksman'] = {
-    cmd = { 'marksman', 'server' },
-    filetypes = { "markdown", "markdown.mdx" },
-    root_markers = { ".marksman.toml", ".git" }
-}
--- nvim/.config/nvim/lua/config/remap.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
-vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
