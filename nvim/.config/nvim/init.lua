4676129af5c6d0b96e7c81e9d2746eaf263445bf
jramber <jdramirezbernal@gmail.com>
2025-11-10 16:20:13 +0100

    remove auto tag

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -81,12 +81,7 @@ vim.pack.add {
     { src = 'https://github.com/kdheepak/lazygit.nvim' },
     -- Docker
     -- 'https://github.com/jesseduffield/lazydocker'
-    -- HTML
-    { src ="https://github.com/windwp/nvim-ts-autotag" },
 }
-
-
-require'nvim-ts-autotag'.setup {}
 
 require'gitsigns'.setup {
     current_line_blame = true,
